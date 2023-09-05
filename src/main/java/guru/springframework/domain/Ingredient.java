c18c8413d9bcd7aaa6d5e03cb141cb92d77d29a6
enlu <nicholas.lugo22@gmail.com>
2023-09-04 22:18:28 -0600

    Adds UnitOfMeasure class and uni-directional 1-1 relationship with Ingredient class.

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -15,6 +15,18 @@ public class Ingredient {
     @ManyToOne
     private Recipe recipe;
 
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
+    }
+
     public Long getId() {
         return id;
     }
